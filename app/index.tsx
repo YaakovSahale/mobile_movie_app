9f97d5ff9d561886703a7f6fcb712e330e1f522d
YaakovSahale <yaakov.sahale@gmail.com  git config --global user.email you@example.com>
2025-09-04 23:47:21 +0300

    first route

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-dark-100 font-bold">
-        Hello!</Text>
+        Hello! yes sirrry</Text>
+      <Link href="/onboarding">Go to Onboarding</Link>
+      <Link href={{ pathname: "/movie/[id]", params: { id: "avengers" } }}>Go to Movie Details</Link>
     </View>
   );
 }
---
+++ app/movie/[id].tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Details = () => {
+    const {id} = useLocalSearchParams()
+
+  return (
+    <View>
+      <Text>Movie Details: {id}</Text>
+    </View>
+  )
+}
+
+export default Details
+
+const styles = StyleSheet.create({})
